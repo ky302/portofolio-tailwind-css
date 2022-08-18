bfa00a1c7ca8859dedd61f39f5c499b4dbb205a1
ahmadsubekhi <ahmadsubekhi550@gmail.com>
2022-08-18 23:37:59 +0800

    portofolio tailwind css

---
+++ src/data/project/ProjectData.js
@@ -0,0 +1,49 @@
+import film from '../../components/assets/porto/film.png'
+import company from '../../components/assets/porto/company.png'
+import kri from '../../components/assets/porto/kri.jpeg'
+import landing from '../../components/assets/porto/landing.png'
+import portal from '../../components/assets/porto/portal.png'
+import smart from '../../components/assets/porto/smart.png'
+import vpn from '../../components/assets/porto/vpn1.png'
+
+
+export const projectData = [
+    {
+        title: 'Film Website',
+        describe: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, modi.',
+        image : film
+
+    },
+    {
+        title: 'Company Profile',
+        describe: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, modi.',
+        image : company
+
+    },
+    {
+        title: 'Landing Page',
+        describe: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, modi.',
+        image : landing
+        
+
+    },
+    {
+        title: 'Portal Job',
+        describe: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, modi.',
+        image : portal
+
+    },
+    {
+        title: 'Smart Card System',
+        describe: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, modi.',
+        image : smart
+
+    },
+    {
+        title: 'Vpn Website',
+        describe: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, modi.',
+        image : vpn
+
+    }
+
+]
